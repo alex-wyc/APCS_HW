93dd5afa4b181745bdc0d8e6a85f369347b902f0
Yicheng Wang <alex.wyc2098@protonmail.ch>
2014-09-15 19:27:27 -0400

    Notes updated

--- 02-greeter/Greeter.java
+++ 02-greeter/Greeter.java
@@ -3,11 +3,17 @@ import java.util.*;
 
 public class Greeter {
 
-    public void greeter() {
+    public void greet() {
 
-	// Simple hello world program
-	System.out.println("Hello World!")
+		// Simple hello world program
+		System.out.println("Hello World!");
+		ungreet();
     
     }
+
+	private void ungreet() {
+		// private = only accessed from within the class
+		System.out.println("I'm out");
+	}
 
 }
