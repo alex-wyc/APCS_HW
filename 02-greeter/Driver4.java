9f994ef5e165a312bd66ca6f18ae134eb28622e2
Yicheng Wang <alex.wyc2098@protonmail.ch>
2014-09-17 09:04:36 -0400

    Calling/assigning variables w/i an object

---
+++ 02-greeter/Driver4.java
@@ -0,0 +1,9 @@
+public class Driver4 {
+	public static void main(String[] args) {
+		Greeter4 g1 = new Greeter4();
+		Greeter4 g2 = new Greeter4();
+		g1.greeting = new String("Bonjour!");
+		g1.greet();
+		g2.greet();
+	}
+}
