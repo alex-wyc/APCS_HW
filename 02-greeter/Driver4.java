66539aee1b7db9851a8b874a18818dce9aaacbcd
Yicheng Wang <alex.wyc2098@protonmail.ch>
2014-09-18 09:11:46 -0400

    Getter + setter function included in note as well as program

--- 02-greeter/Driver4.java
+++ 02-greeter/Driver4.java
@@ -2,8 +2,11 @@ public class Driver4 {
 	public static void main(String[] args) {
 		Greeter4 g1 = new Greeter4();
 		Greeter4 g2 = new Greeter4();
-		g1.greeting = new String("Bonjour!");
+		//g1.greeting = new String("Bonjour!");
+		g1.setGreeting("Bonjour");
 		g1.greet();
 		g2.greet();
+
+		System.out.println("g1's greeting is: " + g1.getGreeting());
 	}
 }
--- 02-greeter/Greeter4.java
+++ 02-greeter/Greeter4.java
@@ -1,8 +1,14 @@
 public class Greeter4 {
-	public String greeting = new String ("Hello World!");
+	private String greeting = new String ("Hello World!");
 	public void greet() {
 		//	String greeting = new String("Sup!");
 		System.out.println(greeting);
+	}
+	public String getGreeting() {
+		return greeting;
+	}
+	public void setGreeting(String s) {
+		greeting = s;
 	}
 	public void ungreet() {
 		System.out.println("I'm out!'");	
