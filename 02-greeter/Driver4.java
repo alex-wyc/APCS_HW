826e111f54a0fdf4c97af191b9ad2222ce1159fd
Yicheng Wang <alex.wyc2098@protonmail.ch>
2014-09-22 09:32:47 -0400

    Final driver/greeter pair written

--- 02-greeter/Driver4.java
+++ 02-greeter/Driver4.java
@@ -1,9 +1,9 @@
 public class Driver4 {
 	public static void main(String[] args) {
-		Greeter4 g1 = new Greeter4();
-		Greeter4 g2 = new Greeter4();
+		Greeter4 g1 = new Greeter4("Bonjour!");
+		Greeter4 g2 = new Greeter4("Sup!");
 		//g1.greeting = new String("Bonjour!");
-		g1.setGreeting("Bonjour");
+		//g1.setGreeting("Bonjour");
 		g1.greet();
 		g2.greet();
 
--- 02-greeter/Greeter4.java
+++ 02-greeter/Greeter4.java
@@ -1,5 +1,13 @@
 public class Greeter4 {
-	private String greeting = new String ("Hello World!");
+	private String greeting; // = new String ("Hello World!");
+	
+	public Greeter4(String s) {
+		greeting = s;
+	}
+	public Greeter4() {
+		greeting = "Hello World!"; // This can be done instead of new String
+								   // This is SLIGHTLY different
+	}
 	public void greet() {
 		//	String greeting = new String("Sup!");
 		System.out.println(greeting);
